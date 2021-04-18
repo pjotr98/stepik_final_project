94db9145b0c3ad62da5b068b0455e18fe6d6aaa8
Oleksandr Zheliazkow <pjotr98@gmail.com>
2021-04-18 13:25:33 +0300

    Check tests independence from content

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,17 @@
 from .pages.product_page import ProductPage
 
-
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_appear_product_in_basket_alert()
+    page.should_be_equivalent_basket_and_products_totals()
+
+
+def test_guest_can_add_product_to_basket_2(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.click_add_to_basket()
